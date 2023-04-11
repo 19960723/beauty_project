4209de8674477fb6ea508aeed577dbfea2218772
lxl <1847426505@qq.com>
2023-04-11 16:36:58 +0800

    基础项目搭建

---
+++ beauty-platform-manage/public/static/config.js
@@ -0,0 +1,11 @@
+/**
+ * 存放配置常量(当值不为空时会覆盖env配置)
+ */
+window._CONFIG = {
+  //接口父路径
+  VUE_APP_API_BASE_URL: 'https://api.boot.jeecg.com',
+  //单点登录地址
+  VUE_APP_CAS_BASE_URL: 'https://api.boot.jeecg.com',
+  //文件预览路径
+  VUE_APP_ONLINE_BASE_URL: ''
+}

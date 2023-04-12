6e1db574f2c6e746acac5c17e1110bbb229a13e4
lxl <1847426505@qq.com>
2023-04-12 17:43:05 +0800

    commit

--- beauty-user-small/pages/index/index.vue
+++ beauty-user-small/pages/index/index.vue
@@ -1,8 +1,20 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view>
+		<home :cur="PageCur" v-if="PageCur=='home'" :key="commponent1Key"></home>
+		<people v-if="PageCur=='people'" :key="commponent2Key"></people>
+		<view class="cu-bar tabbar bg-white shadow foot">
+			<view :class="PageCur=='home'?'action text-green':'action text-gray'" @click="NavChange" data-cur="home">
+				<view class='cuIcon-homefill'></view>主页
+			</view>
+			<view :class="PageCur=='peoplelis'?'action text-green':'action text-gray'" @click="NavChange" data-cur="peoplelis">
+				<view class='cuIcon-peoplelist'></view>审批
+			</view>
+			<view :class="PageCur=='profile'?'action text-green':'action text-gray'" @click="NavChange" data-cur="profile">
+				<view class='cuIcon-profile'></view>发起
+			</view>
+			<view :class="PageCur=='people'?'action text-green':'action text-gray'" @click="NavChange" data-cur="people">
+				<view class='cuIcon-people'></view>个人
+			</view>
 		</view>
 	</view>
 </template>
@@ -10,43 +22,26 @@
 <script>
 	export default {
 		data() {
-			return {
-				title: 'Hello'
+		return {
+				PageCur: 'home',
+				commponent1Key: 0,
+				commponent2Key: 0,
 			}
 		},
-		onLoad() {
-
+		onLoad:function(){
+			this.PageCur='home'
+			++this.commponent1Key
+			++this.commponent2Key
 		},
 		methods: {
-
+			NavChange: function(e) {
+				this.PageCur = e.currentTarget.dataset.cur
+			}
+			
 		}
 	}
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
 </style>
